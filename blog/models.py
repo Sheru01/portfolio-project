66461aa6c871ac36d8707217d5b098fea69412ef
M.S. Rana <shehraanrana@gmail.com>
2020-10-06 19:00:33 -0400

    This is my second

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create a blog model
 class Blog(models.Model):
-
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
