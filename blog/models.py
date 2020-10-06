706fdd666c0db43b00403fa7f4bfc61cc6cfff3c
M.S. Rana <shehraanrana@gmail.com>
2020-10-06 17:45:02 -0400

    jobs app up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog model
+class Blog(models.Model):
+
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# Add blog apps to settings
+
+
+
+# create a migration
+
+# Migrate
+
+#Add to the admin
